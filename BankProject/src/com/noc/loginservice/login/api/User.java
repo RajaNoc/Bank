09ce3226292d9037a5644f9e086d2c2640c4422e
RajaNoc <pmt.seti@yahoo.com>
2017-04-25 07:32:36 +0530

    modified for password encryption logic

--- BankProject/src/com/noc/loginservice/login/api/User.java
+++ BankProject/src/com/noc/loginservice/login/api/User.java
@@ -1,3 +1,4 @@
+
 package com.noc.loginservice.login.api;
 
 public class User {
@@ -9,6 +10,26 @@ public class User {
 	public String conformPassword;
 	//For one time password for registration confirmation
 	public int otp;
+	puclic String encryptPassword(String password){
+		try {
+        md = MessageDigest.getInstance("MD5");
+        byte[] passBytes = pass.getBytes();
+        md.reset();
+        byte[] digested = md.digest(passBytes);
+        StringBuffer sb = new StringBuffer();
+        for(int i=0;i<digested.length;i++){
+            sb.append(Integer.toHexString(0xff & digested[i]));
+        }
+        return sb.toString();
+    } catch (NoSuchAlgorithmException ex) {
+        Logger.getLogger(CryptWithMD5.class.getName()).log(Level.SEVERE, null, ex);
+    }
+        return null;
+
+
+   }
+
+	}
 	public String getUserName() {
 		return userName;
 	}
